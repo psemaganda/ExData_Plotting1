0d0047cdc02f0b693e8cf037b29a3b3eae795189
psemaganda <psemaganda@gmail.com>
2015-02-05 22:57:57 +0100

    Final commit
    made changes to plot4.png, and plot4.R

--- plot4.R
+++ plot4.R
@@ -1,15 +1,15 @@
 ## Read data from "household_power_consumption.txt" 
 ## located in the working directory on your computer
 
-EPC <- read.table("household_power_consumption.txt", header = T, sep = ';', na.strings = "?")
-
-## Subseting the data to include only data from the dates 2007-02-01 and 2007-02-02.
-
-EPC <- EPC[EPC$Date == "2007-02-01" | EPC$Date == "2007-02-02", ]
+EPC <- read.table("~/data/household_power_consumption.txt", header = T, sep = ';', na.strings = "?")
 
 ## Transforming the Date variable into a date object
 
 EPC$Date <- as.Date(x = EPC$Date, format = "%d/%m/%Y")
+
+## Subseting the data to include only data from the dates 2007-02-01 and 2007-02-02.
+
+EPC <- EPC[EPC$Date == "2007-02-01" | EPC$Date == "2007-02-02", ]
 
 ## Transforming the Time variable into a POSIXct object
 
@@ -22,13 +22,13 @@ par(mfcol = c(2,2))
 
 ## plot1
 plot(EPC$Time, EPC$Global_active_power, xlab = "", 
-     ylab = "Global Active Power (kilowatts)", type = 'l')
+     ylab = "Global Active Power", type = 'l')
 
 ## plot2
 plot(EPC$Time,EPC$Sub_metering_1,xlab = "", ylab = "Energy sub metering", type = 'l')
 points(EPC$Time,EPC$Sub_metering_2, type = 'l', col = 'red')
 points(EPC$Time,EPC$Sub_metering_3, type = 'l', col = 'blue')
-legend('topright',legend = c('Sub_metering_1','Sub_metering_2', 'Sub_metering_3'), 
+legend('topright',legend = c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), bty = 'n', 
        lty = 1, col = c('black','red','blue'))
 
 ## plot3
